7fd7cf0d71e1f72b909442c265c5148ed29b95e5
marmeladema <xademax@gmail.com>
2021-02-20 23:55:08 +0000

    Add key weight measurement

--- src/config.rs
+++ src/config.rs
@@ -5,15 +5,15 @@ use std::marker::PhantomData;
 use std::num::NonZeroUsize;
 
 /// A configuration structure used to create an LRU cache.
-pub struct CLruCacheConfig<V, S = RandomState, W = ZeroWeightScale> {
+pub struct CLruCacheConfig<K, V, S = RandomState, W = ZeroWeightScale> {
     pub(crate) capacity: NonZeroUsize,
     pub(crate) hash_builder: S,
     pub(crate) reserve: Option<usize>,
     pub(crate) scale: W,
-    _marker: PhantomData<V>,
+    _marker: PhantomData<(K, V)>,
 }
 
-impl<V> CLruCacheConfig<V> {
+impl<K, V> CLruCacheConfig<K, V> {
     /// Creates a new configuration that will create an LRU cache
     /// that will hold at most `capacity` elements and default parameters.
     pub fn new(capacity: NonZeroUsize) -> Self {
@@ -27,9 +27,9 @@ impl<V> CLruCacheConfig<V> {
     }
 }
 
-impl<V, S: BuildHasher, W: WeightScale<V>> CLruCacheConfig<V, S, W> {
+impl<K, V, S: BuildHasher, W: WeightScale<K, V>> CLruCacheConfig<K, V, S, W> {
     /// Configure the provided hash builder.
-    pub fn with_hasher<O: BuildHasher>(self, hash_builder: O) -> CLruCacheConfig<V, O, W> {
+    pub fn with_hasher<O: BuildHasher>(self, hash_builder: O) -> CLruCacheConfig<K, V, O, W> {
         let Self {
             capacity,
             reserve,
@@ -54,7 +54,7 @@ impl<V, S: BuildHasher, W: WeightScale<V>> CLruCacheConfig<V, S, W> {
     }
 
     /// Configure the provided scale.
-    pub fn with_scale<O: WeightScale<V>>(self, scale: O) -> CLruCacheConfig<V, S, O> {
+    pub fn with_scale<O: WeightScale<K, V>>(self, scale: O) -> CLruCacheConfig<K, V, S, O> {
         let Self {
             capacity,
             hash_builder,
